1703a1f841ccbf72c9065363e46fae602cb73809
SARAHYI <dltmfrl0608@gmail.com>
2026-04-08 21:22:29 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1) / 2];
+        int count = 0;
+        
+        for(int i = 0; i<=n; i++){
+            if (i % 2 == 1){   
+              answer[count] = i;
+              count++;
+            }
+        }
+        return answer;
+    }
+}
