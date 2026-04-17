013b1ef1a7cc5b7724fe0a2abf8b025d21c50445
SARAHYI <dltmfrl0608@gmail.com>
2026-04-17 21:26:09 +0900

    [Bronze V] Title: 개, Time: 92 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");    // \"를 사용해 큰따옴표 출력, \\로 백슬래시 출력
+        System.out.println("|\"^\"`    |");    // 여기도 \"를 사용
+        System.out.println("||_/=\\\\__|");     // 백슬래시가 두 개면 \\\\로
+    }
+}
