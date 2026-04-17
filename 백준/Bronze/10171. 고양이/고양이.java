e2c03069a8c613c85e54e85c8b536d6b418abf77
SARAHYI <dltmfrl0608@gmail.com>
2026-04-17 21:21:22 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 백슬래시 하나를 출력하기 위해 \\를 사용합니다.
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
